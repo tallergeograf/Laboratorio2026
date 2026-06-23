126bce0563b28f40bc22745007e725d300ed9894
Bruno <bruaguspons@gmail.com>
2026-06-23 19:38:04 -0300

    fix: disable SSL verification for direcciones.ide.uy (AGESIC CA not in JVM truststore)

--- backend/src/main/java/uy/edu/taller/sige/geo_api/config/RestTemplateConfig.java
+++ backend/src/main/java/uy/edu/taller/sige/geo_api/config/RestTemplateConfig.java
@@ -1,4 +1,14 @@
 package uy.edu.taller.sige.geo_api.config;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
@@ -12,7 +22,20 @@ public class RestTemplateConfig {
     private static final int READ_TIMEOUT_MS    = 20_000;
 
     @Bean
-    public RestTemplate restTemplate() {
+    public RestTemplate restTemplate() throws NoSuchAlgorithmException, KeyManagementException {
+        // direcciones.ide.uy uses an AGESIC (Uruguayan gov) CA not trusted by the default JVM truststore.
+        TrustManager[] trustAll = new TrustManager[] {
+            new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+            }
+        };
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustAll, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+        HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
         factory.setConnectTimeout(CONNECT_TIMEOUT_MS);
         factory.setReadTimeout(READ_TIMEOUT_MS);
